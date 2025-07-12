45ece64691c48cad802d3e54a835fb97d39c1c2b
havancuong2003 <havancuong07122003@gmail.com>
2025-07-12 10:52:01 +0700

    m: update account manager

--- client/src/pages/admin/admin.home/AdminHome.tsx
+++ client/src/pages/admin/admin.home/AdminHome.tsx
@@ -1,16 +1,17 @@
 // pages/admin/accounts/index.tsx
 import { useEffect, useState } from "react";
-import { TextField, Table, TableBody, TableCell, TableHead, TableRow, Select, MenuItem, InputLabel } from "@mui/material";
+import { TextField, Table, TableBody, TableCell, TableHead, TableRow, Select, MenuItem, InputLabel, Link } from "@mui/material";
 import type { AccountListItem } from "../../../model/Interface";
 import { getAccounts } from "../../../services/admin.service";
+import { useNavigate } from "react-router-dom";
 
 
 export default function AccountHomePage() {
   const [accounts, setAccounts] = useState<AccountListItem[]>([]);
-
+  const navigate = useNavigate();
   const [search, setSearch] = useState("");
   const [filterRole, setFilterRole] = useState("all");
-
+  const [accountDetail, setAccountDetail] = useState<AccountListItem | null>(null);
 
   useEffect(() => {
     const fetchAccounts = async () => {
@@ -45,11 +46,13 @@ export default function AccountHomePage() {
         <MenuItem value="all">Tất cả</MenuItem>
         <MenuItem value="parent">Phụ huynh</MenuItem>
         <MenuItem value="teacher">Giáo viên</MenuItem>
+
       </Select>
 
       <Table>
         <TableHead>
           <TableRow>
+         
             <TableCell>Họ tên</TableCell>
             <TableCell>Email</TableCell>
             <TableCell>Vai trò</TableCell>
@@ -59,7 +62,9 @@ export default function AccountHomePage() {
         <TableBody>
           {filteredAccounts?.map(acc => (
             <TableRow key={acc._id} >
-              <TableCell>{acc.fullName}</TableCell>
+              <TableCell sx={{cursor: 'pointer'}} onClick={() => navigate(`/admin-home/account-management/${acc._id}`)}>
+                {acc.fullName}
+              </TableCell>
               <TableCell>{acc.email}</TableCell>
               <TableCell>{acc.role}</TableCell>
               <TableCell>{acc.phoneNumber}</TableCell>
--- client/src/services/admin.service.tsx
+++ client/src/services/admin.service.tsx
@@ -11,3 +11,23 @@ export const getAccounts = async () => {
         throw error;
     }
 };
+
+export const getAccountById = async (id: string) => {
+    try {
+        const response = await axiosInstance.get(`/admin/accounts/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error("Get account by id failed:", error);
+        throw error;
+    }
+};
+
+export const updateAccount = async (id: string, data: any) => {
+    try {
+        const response = await axiosInstance.put(`/admin/accounts/${id}`, data);
+        return response.data;
+    } catch (error) {
+        console.error("Update account failed:", error);
+        throw error;
+    }
+};
